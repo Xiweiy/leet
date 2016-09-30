d83d5883bed69125a8a7d77043e6d3a57ce0b187
Xiweiy <athenayxw@gmail.com>
2016-09-30 00:32:20 -0700

    Binary Tree Inorder Traversal: recursive solution

---
+++ algm_94.py
@@ -0,0 +1,6 @@
+##SOLUTION 1: RECURSIVE SOLUTION
+class Solution(object):
+    def inorderTraversal(self, root):
+        if not root:
+            return []
+        return self.inorderTraversal(root.left) + [root.val] + self.inorderTraversal(root.right)
