698680a35f5d7b3b82823449977479fe51881cef
Xiweiy <athenayxw@gmail.com>
2016-09-30 23:47:10 -0700

    Binary Tree Inorder Traversal: iterative solution

--- algm_94.py
+++ algm_94.py
@@ -4,3 +4,29 @@ class Solution(object):
         if not root:
             return []
         return self.inorderTraversal(root.left) + [root.val] + self.inorderTraversal(root.right)
+
+
+##SOLUTION 2: ITERATIVE SOLUTION
+class Solution(object):
+    def inorderTraversal(self, root):
+        if not root:
+            return []
+        nodelist = [root]
+        visited = [0]
+        inorder = []
+        while nodelist:
+            if visited[-1]:  ##Left child added
+                curparent = nodelist.pop()
+                visited.pop()
+                inorder.append(curparent.val)
+                nodelist.append(curparent.right)
+                visited.append(0)
+            elif not nodelist[-1]:   ##Node is None
+                nodelist.pop()
+                visited.pop()
+            else:      ##Before left child added
+                visited[-1] += 1
+                nodelist.append(nodelist[-1].left)
+                visited.append(0)
+                
+        return inorder
