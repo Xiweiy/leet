d9b8401ad92f4f7cf2b0f1f783fd7c6e07faab1c
Xiweiy <athenayxw@gmail.com>
2016-10-03 13:38:19 -0700

    binary tree preorder traversal: morris traversal

--- algm_144.py
+++ algm_144.py
@@ -28,3 +28,35 @@ class Solution(object):
                 nodelist.append(nodelist[-1].left)
                 visited.append(0)
         return inorder
+
+
+##SOLUTION 3: Morris Traversal
+class Solution(object):
+    def find_mostright(self, root):
+        if not root.left.right:
+            return root.left
+        else:
+            curparent = root.left
+            while curparent.right and curparent.right != root:
+                curparent = curparent.right
+            return curparent
+        
+    def preorderTraversal(self, root):
+        if not root:
+            return []
+        curnode = root
+        inorder = []
+        while curnode:
+            if not curnode.left: 
+                inorder.append(curnode.val)
+                curnode = curnode.right
+            else:
+                rightmost = self.find_mostright(curnode)
+                if rightmost.right == curnode:
+                    rightmost.right = None
+                    curnode = curnode.right
+                else:
+                    inorder.append(curnode.val)
+                    rightmost.right = curnode
+                    curnode = curnode.left
+        return inorder
