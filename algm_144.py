bb67512a9ee6b6ce50510298136d097d5b638fec
Xiweiy <athenayxw@gmail.com>
2016-10-03 11:36:05 -0700

    binary tree preorder traversal: recursive solution

---
+++ algm_144.py
@@ -0,0 +1,6 @@
+##SOLUTION 1: Recursive Solution
+class Solution(object):
+    def preorderTraversal(self, root):
+        if not root:
+            return []
+        return [root.val] + self.preorderTraversal(root.left) + self.preorderTraversal(root.right)
