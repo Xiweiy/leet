9f5214e92fa015e61c2d21c8986f1c92c303cb63
Xiwei Yan <xiweiyan@Siddharths-MacBook-Pro.local>
2018-01-01 22:20:09 -0800

    intersection of two arrays: add solution 2-when set operation not allowed

--- algm_349.py
+++ algm_349.py
@@ -1,4 +1,28 @@
+##SOLUTION 1: SET
 class Solution(object):
     def intersection(self, nums1, nums2):
         numdic = {i:0 for i in nums1}
         return list(set([i for i in nums2 if i in numdic]))
+
+##SOLUTION 2: WHEN SET NOT ALLOWED
+class Solution(object):
+    def intersection(self, nums1, nums2):
+        nums1 = sorted(nums1)
+        nums2 = sorted(nums2)
+        i,j = 0,0
+        m,n = len(nums1), len(nums2)
+        resultlist = []
+        while i<m and j<n:
+            print i,j
+            if nums1[i]<nums2[j]:
+                i += 1
+            elif nums1[i]>nums2[j]:
+                j += 1
+            else:
+                if not resultlist or resultlist[-1]!= nums1[i]:
+                    resultlist.append(nums1[i])
+                i += 1
+                j += 1
+        return resultlist
+
+##NOTE: similar to merge sort
